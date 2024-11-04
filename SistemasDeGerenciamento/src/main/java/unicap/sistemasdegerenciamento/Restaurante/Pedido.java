9afc96894951a227246e4e7017d828ae89c07e2d
PheonixEdson <135137092+PheonixEdson@users.noreply.github.com>
2024-11-04 20:16:16 -0300

    Update Pedido.java

--- SistemasDeGerenciamento/src/main/java/unicap/sistemasdegerenciamento/Restaurante/Pedido.java
+++ SistemasDeGerenciamento/src/main/java/unicap/sistemasdegerenciamento/Restaurante/Pedido.java
@@ -5,14 +5,16 @@ import java.util.List;
 
 public class Pedido {
     private int id;
+    private String nome;  
     private Mesa mesa;
     private List<ItemDoPedido> itens;
     private double total;
     private boolean fechado;
     private List<Pedido> pedidos;
 
-    public Pedido(int id, Mesa mesa) {
+    public Pedido(int id, String nome, Mesa mesa) {
         this.id = id;
+        this.nome = nome; 
         this.mesa = mesa;
         this.itens = new ArrayList<>();
         this.total = 0.0;
@@ -20,124 +22,127 @@ public class Pedido {
         pedidos = new ArrayList<>();
     }
 
-    public void adicionarItem(ItemDoPedido item){
-        if(fechado == false){
+    public void adicionarItem(ItemDoPedido item) {
+        if (!fechado) {
             itens.add(item);
             total += item.calcularPrecoTotal();
-        }
-        else{
-            System.out.println("O pedido ja foi fechado!");
+        } else {
+            System.out.println("O pedido já foi fechado!");
         }
     }
 
     public void removerItem(String nomeItem, int quantidade) {
-        if(fechado == false){
+        if (!fechado) {
             ItemDoPedido itemARemover = null;
 
-            for(ItemDoPedido item : itens){
-                if(item.getNome().equalsIgnoreCase(nomeItem)){
+            for (ItemDoPedido item : itens) {
+                if (item.getNome().equalsIgnoreCase(nomeItem)) {
                     itemARemover = item;
                     break;
                 }
             }
 
-            if(itemARemover != null){
-                if(itemARemover.getQuantidade() > quantidade){
+            if (itemARemover != null) {
+                if (itemARemover.getQuantidade() > quantidade) {
                     itemARemover.setQuantidade(itemARemover.getQuantidade() - quantidade);
                     total -= itemARemover.getPreco() * quantidade;
-                    System.out.println(quantidade+ " unidade(s) de " +nomeItem+ "(s) removida(s) com sucesso.");
-                }
-                else {
+                    System.out.println(quantidade + " unidade(s) de " + nomeItem + "(s) removida(s) com sucesso.");
+                } else {
                     itens.remove(itemARemover);
                     total -= itemARemover.calcularPrecoTotal();
-                    System.out.println("Todas as unidades de " +nomeItem+ " foram removidas.");
+                    System.out.println("Todas as unidades de " + nomeItem + " foram removidas.");
                 }
+            } else {
+                System.out.println("Item não encontrado.");
             }
-            else {
-                System.out.println("Item nao encontrado.");
-            }
-        }
-        else {
-            System.out.println("O pedido ja foi fechado!");
+        } else {
+            System.out.println("O pedido já foi fechado!");
         }
     }
-    
+
     public void adicionarPedido(Pedido pedido) {
         pedidos.add(pedido);
         System.out.println("Pedido ID " + pedido.getId() + " adicionado com sucesso.");
     }
-    
-    public void cancelarPedido(){
-        if(fechado == false){
+
+    public void cancelarPedido() {
+        if (!fechado) {
             itens.clear();
             total = 0.0;
             System.out.println("O seu pedido foi cancelado. Realize um novo pedido.");
-        } 
-        else{
-            System.out.println("O pedido já foi fechado e nao pode ser cancelado.");
+        } else {
+            System.out.println("O pedido já foi fechado e não pode ser cancelado.");
         }
     }
-    
-    public void fecharPedido(){
+
+    public void fecharPedido() {
         this.fechado = true;
-        System.out.println("\n>>Conta Fechada<<");
+        System.out.println("\n>> Conta Fechada <<");
         System.out.println("Conta Final:");
         this.gerarRelatorio();
     }
-    
-    public void reabrirPedido(){
+
+    public void reabrirPedido() {
         this.fechado = false;
     }
-    
-    public void gerarRelatorio(){
+
+    public void gerarRelatorio() {
         System.out.println("Pedido ID: " + this.getId());
+        System.out.println("Nome do Pedido: " + this.getNome());  
         System.out.println("Mesa: " + this.mesa.getNumero());
         System.out.println("------------------");
         System.out.println("Pedido do Cliente:");
 
-        for(ItemDoPedido item : this.itens){
-            System.out.println(">> " +item.getNome()+ " <<");
-            System.out.println("+Quantidade: " +item.getQuantidade());
-            System.out.println("+Preco Unitario: R$" +item.getPreco());
-            System.out.println("+Preco Total: R$" +item.calcularPrecoTotal());
+        for (ItemDoPedido item : this.itens) {
+            System.out.println(">> " + item.getNome() + " <<");
+            System.out.println("+ Quantidade: " + item.getQuantidade());
+            System.out.println("+ Preço Unitário: R$" + item.getPreco());
+            System.out.println("+ Preço Total: R$" + item.calcularPrecoTotal());
         }
 
-        System.out.println("#Total a pagar: R$" +this.total);
-        
-        if(this.fechado == true){
+        System.out.println("# Total a pagar: R$" + this.total);
+
+        if (this.fechado) {
             System.out.println("Conta fechada");
-        }
-        else{
+        } else {
             System.out.println("Conta aberta");
         }
     }
-    
+
     public Pedido buscarPedidoPorId(int id) {
         for (Pedido pedido : pedidos) {
             if (pedido.getId() == id) {
                 return pedido;
             }
         }
-        System.out.println("Pedido com ID " + this.getId() + " não encontrado.");
+        System.out.println("Pedido com ID " + id + " não encontrado.");
         return null;
     }
 
-    public int getId(){
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getId() {
         return id;
     }
 
-    public Mesa getMesa(){
+    public Mesa getMesa() {
         return mesa;
     }
 
-    public List<ItemDoPedido> getItens(){
+    public List<ItemDoPedido> getItens() {
         return itens;
     }
-    
+
     public List<Pedido> getPedidos() {
         return pedidos;
     }
-    
+
     public double getTotal() {
         return total;
     }
@@ -146,7 +151,7 @@ public class Pedido {
         this.total = total;
     }
 
-    public boolean isFechado(){
+    public boolean isFechado() {
         return fechado;
     }
 }
