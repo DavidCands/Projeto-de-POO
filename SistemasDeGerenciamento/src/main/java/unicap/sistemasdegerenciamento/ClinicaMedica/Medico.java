b5e3851a78f135770aaa06d59ad7ceb63938aa36
PheonixEdson <135137092+PheonixEdson@users.noreply.github.com>
2024-11-12 20:48:24 -0300

    Update Medico.java

--- SistemasDeGerenciamento/src/main/java/unicap/sistemasdegerenciamento/ClinicaMedica/Medico.java
+++ SistemasDeGerenciamento/src/main/java/unicap/sistemasdegerenciamento/ClinicaMedica/Medico.java
@@ -1,40 +1,29 @@
 package unicap.sistemasdegerenciamento.ClinicaMedica;
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
 
 public class Medico {
     private String nome;
     private String especialidade;
-    private List<LocalDateTime> agendaDisponivel;  
-
+    private boolean disponivel;
+    
     public Medico(String nome, String especialidade) {
         this.nome = nome;
         this.especialidade = especialidade;
-        this.agendaDisponivel = new ArrayList<>(); 
-        preencherAgenda();  
+        this.disponivel = true;
     }
-
+    
     public String getNome() {
         return nome;
     }
-
+    
     public String getEspecialidade() {
         return especialidade;
     }
-
-    public List<LocalDateTime> getAgendaDisponivel() {
-        return agendaDisponivel;
+    
+    public boolean isDisponivel() {
+        return disponivel;
     }
-
-    public void removerHorario(LocalDateTime horario) {
-        agendaDisponivel.remove(horario);  
-    }
-
-    private void preencherAgenda() {    
-        LocalDateTime dataInicio = LocalDateTime.now().withHour(8).withMinute(0);  
-        for (int i = 0; i < 8; i++) {  
-            agendaDisponivel.add(dataInicio.plusHours(i));
-        }
+    
+    public void setDisponivel(boolean disponivel) {
+        this.disponivel = disponivel;
     }
 }
