d9bad6565f0bace1735e4b1bbf4b0b21f3577208
PheonixEdson <135137092+PheonixEdson@users.noreply.github.com>
2024-11-26 22:08:46 -0300

    Update Clinica.java

--- SistemasDeGerenciamento/src/main/java/unicap/sistemasdegerenciamento/ClinicaMedica/Clinica.java
+++ SistemasDeGerenciamento/src/main/java/unicap/sistemasdegerenciamento/ClinicaMedica/Clinica.java
@@ -5,54 +5,47 @@ import java.util.Date;
 import java.util.List;
 
 public class Clinica {
-    private List<Paciente> pacientes;
-    private List<Medico> medicos;
+    private List<Pessoa> pessoas;
     private List<Consulta> consultas;
-    
+
     public Clinica() {
-        pacientes = new ArrayList<>();
-        medicos = new ArrayList<>();
+        pessoas = new ArrayList<>();
         consultas = new ArrayList<>();
     }
-    
-    public void adicionarPaciente(Paciente paciente) {
-        pacientes.add(paciente);
+
+    public void adicionarPessoa(Pessoa pessoa) {
+        pessoas.add(pessoa);
     }
-    
-    public void adicionarMedico(Medico medico) {
-        medicos.add(medico);
-    }
-    
+
     public Medico buscarMedicoPorNome(String nome) {
-        for (Medico medico : medicos) {
-            if (medico.getNome().equalsIgnoreCase(nome)) {
-                return medico;
+        for (Pessoa pessoa : pessoas) {
+            if (pessoa instanceof Medico && pessoa.getNome().equalsIgnoreCase(nome)) {
+                return (Medico) pessoa;
             }
         }
         return null;
     }
-    
+
     public Paciente buscarPacientePorNome(String nome) {
-        for (Paciente paciente : pacientes) {
-            if (paciente.getNome().equalsIgnoreCase(nome)) {
-                return paciente;
+        for (Pessoa pessoa : pessoas) {
+            if (pessoa instanceof Paciente && pessoa.getNome().equalsIgnoreCase(nome)) {
+                return (Paciente) pessoa;
             }
         }
         return null;
     }
-    
+
     public void agendarConsulta(Paciente paciente, Medico medico, Date data) {
         if (medico.isDisponivel()) {
             Consulta consulta = new Consulta(paciente, medico, data);
             consultas.add(consulta);
             medico.setDisponivel(false);
             System.out.println("Consulta agendada com sucesso!");
-        } 
-        else {
+        } else {
             System.out.println("Médico não está disponível.");
         }
     }
-    
+
     public void buscarConsultas() {
         System.out.println("Relatório de Consultas:");
         for (Consulta consulta : consultas) {
@@ -60,8 +53,9 @@ public class Clinica {
         }
     }
 
-    public void main(String[] strings) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'main'");
+    public void exibirInformacoesPessoas() {
+        for (Pessoa pessoa : pessoas) {
+            pessoa.exibirInformacoes();  
+        }
     }
 }
