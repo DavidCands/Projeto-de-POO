7c4e4aa36c760431c5a1b1e1d5290df4c4adf155
PheonixEdson <135137092+PheonixEdson@users.noreply.github.com>
2024-11-26 22:10:27 -0300

    Update Pessoa.java

--- SistemasDeGerenciamento/src/main/java/unicap/sistemasdegerenciamento/Pessoa.java
+++ SistemasDeGerenciamento/src/main/java/unicap/sistemasdegerenciamento/Pessoa.java
@@ -1,23 +1,5 @@
 package unicap.sistemasdegerenciamento.ClinicaMedica;
 
-public class Pessoa {
-    private String nome;
-    private String cpf;
-
-    public Pessoa(String nome, String cpf) {
-        this.nome = nome;
-        this.cpf = cpf;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public String getCpf() {
-        return cpf;
-    }
-    
-    public String toString() {
-        return "Nome: " + nome + ", CPF: " + cpf;
-    }
+public interface Pessoa {
+    void exibirInformacoes();
 }
