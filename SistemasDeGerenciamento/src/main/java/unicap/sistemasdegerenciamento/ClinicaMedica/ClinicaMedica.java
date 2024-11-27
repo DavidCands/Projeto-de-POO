e430b70437cddf77798cba1a9f2bd32b9a98ccf7
PheonixEdson <135137092+PheonixEdson@users.noreply.github.com>
2024-11-26 22:07:22 -0300

    Update ClinicaMedica.java

--- SistemasDeGerenciamento/src/main/java/unicap/sistemasdegerenciamento/ClinicaMedica/ClinicaMedica.java
+++ SistemasDeGerenciamento/src/main/java/unicap/sistemasdegerenciamento/ClinicaMedica/ClinicaMedica.java
@@ -1,12 +1,10 @@
 package unicap.sistemasdegerenciamento.ClinicaMedica;
 
-import java.util.Date;
 import java.util.Scanner;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Date;
 
 public class ClinicaMedica {
-    public static void main(String[] args, Clinica clinica) {
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Clinica clinica = new Clinica();
         int opcao;
@@ -24,72 +22,75 @@ public class ClinicaMedica {
                 case 1:
                     System.out.print("Digite o nome do paciente: ");
                     String nomePaciente = scanner.nextLine();
-                    
+
                     System.out.print("Digite o CPF do paciente: ");
                     String cpfPaciente = scanner.nextLine();
-                    
+
                     System.out.println("Digite a idade do paciente: ");
                     int idadePaciente = scanner.nextInt();
-                    
+
                     System.out.println("Digite o email do paciente: ");
                     String emailPaciente = scanner.nextLine();
-                    
+
                     System.out.println("Digite o telefone do paciente: ");
                     String telefonePaciente = scanner.nextLine();
-                    
+
                     Paciente paciente = new Paciente(nomePaciente, cpfPaciente, idadePaciente, emailPaciente, telefonePaciente);
                     clinica.adicionarPaciente(paciente);
-                    
+
                     System.out.println("Paciente adicionado com sucesso!");
                     break;
-                    
+
                 case 2:
                     System.out.print("Digite o nome do medico: ");
                     String nomeMedico = scanner.nextLine();
-                    
+
                     System.out.print("Digite a especialidade do medico: ");
                     String especialidadeMedico = scanner.nextLine();
-                    
-                    Medico medico = new Medico(nomeMedico, especialidadeMedico);
+
+                    Medico medico = new Medico(nomeMedico, "00000000000", especialidadeMedico);
                     clinica.adicionarMedico(medico);
-                    
+
                     System.out.println("Medico adicionado com sucesso!");
                     break;
-                    
+
                 case 3:
                     System.out.print("Digite o nome do paciente: ");
                     String nomePacienteConsulta = scanner.nextLine();
-                    
+
                     Paciente pacienteConsulta = clinica.buscarPacientePorNome(nomePacienteConsulta);
                     if (pacienteConsulta == null) {
                         System.out.println("Paciente nao encontrado.");
                         break;
                     }
-                    
+
                     System.out.print("Digite o nome do medico: ");
                     String nomeMedicoConsulta = scanner.nextLine();
-                    
+
                     Medico medicoConsulta = clinica.buscarMedicoPorNome(nomeMedicoConsulta);
                     if (medicoConsulta == null) {
                         System.out.println("Medico nao encontrado.");
                         break;
                     }
+
                     clinica.agendarConsulta(pacienteConsulta, medicoConsulta, new Date());
                     break;
-                    
+
                 case 4:
-                    clinica.buscarConsultas();
+                    System.out.println("Relatório de Consultas:");
+                    for (Agendavel item : clinica.getConsultas()) {
+                        item.exibirInformacoes();  // Polimorfismo em ação
+                    }
                     break;
-                    
+
                 case 0:
                     System.out.println("Voltando...");
                     break;
-                    
+
                 default:
                     System.out.println("Opcao invalida!");
                     break;
-                    
             }
-        }while(opcao != 0);
+        } while (opcao != 0);
     }
 }
