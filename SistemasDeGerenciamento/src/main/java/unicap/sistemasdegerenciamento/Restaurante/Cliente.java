3adcf013f4d808add5be831f5474a642094136e1
PheonixEdson <135137092+PheonixEdson@users.noreply.github.com>
2024-11-28 19:23:26 -0300

    Update Cliente.java

--- SistemasDeGerenciamento/src/main/java/unicap/sistemasdegerenciamento/Restaurante/Cliente.java
+++ SistemasDeGerenciamento/src/main/java/unicap/sistemasdegerenciamento/Restaurante/Cliente.java
@@ -1,11 +1,19 @@
-package unicap.sistemasdegerenciamento.Restaurante;
+public class ClienteRestaurante implements Pessoa {
+    private String nome;
+    private String cpf;
 
-public class Cliente extends Pessoa {
-
-    public Cliente(String nome, boolean isMedico) {
-        super(nome, isMedico);
+    public ClienteRestaurante(String nome, String cpf) {
+        this.nome = nome;
+        this.cpf = cpf;
     }
+
+    @Override
     public void exibirInformacoes() {
-        System.out.println("Cliente: " + nome);
+        System.out.println(nome + " | CPF: " + cpf);
+    }
+
+    @Override
+    public boolean isMedico() {
+        return false;
     }
 }
