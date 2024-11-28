375636f67d60b480a07e8dac202906d821c7d8d9
PheonixEdson <135137092+PheonixEdson@users.noreply.github.com>
2024-11-28 19:05:36 -0300

    Update Clinica.java

--- SistemasDeGerenciamento/src/main/java/unicap/sistemasdegerenciamento/ClinicaMedica/Clinica.java
+++ SistemasDeGerenciamento/src/main/java/unicap/sistemasdegerenciamento/ClinicaMedica/Clinica.java
@@ -5,31 +5,37 @@ import java.util.Date;
 import java.util.List;
 
 public class Clinica {
-    private List<Pessoa> pessoas;
+    private List<Paciente> pacientes;
+    private List<Medico> medicos;
     private List<Consulta> consultas;
 
     public Clinica() {
-        pessoas = new ArrayList<>();
+        pacientes = new ArrayList<>();
+        medicos = new ArrayList<>();
         consultas = new ArrayList<>();
     }
 
-    public void adicionarPessoa(Pessoa pessoa) {
-        pessoas.add(pessoa);
+    public void adicionarPaciente(Paciente paciente) {
+        pacientes.add(paciente);
+    }
+
+    public void adicionarMedico(Medico medico) {
+        medicos.add(medico);
     }
 
     public Medico buscarMedicoPorNome(String nome) {
-        for (Pessoa pessoa : pessoas) {
-            if (pessoa instanceof Medico && pessoa.getNome().equalsIgnoreCase(nome)) {
-                return (Medico) pessoa;
+        for (Medico medico : medicos) {
+            if (medico.getNome().equalsIgnoreCase(nome)) {
+                return medico;
             }
         }
         return null;
     }
 
     public Paciente buscarPacientePorNome(String nome) {
-        for (Pessoa pessoa : pessoas) {
-            if (pessoa instanceof Paciente && pessoa.getNome().equalsIgnoreCase(nome)) {
-                return (Paciente) pessoa;
+        for (Paciente paciente : pacientes) {
+            if (paciente.getNome().equalsIgnoreCase(nome)) {
+                return paciente;
             }
         }
         return null;
@@ -46,16 +52,24 @@ public class Clinica {
         }
     }
 
+    public void agendarConsulta(Paciente paciente, Medico medico) {
+        agendarConsulta(paciente, medico, new Date());
+    }
+    
+    public void agendarConsulta(String nomePaciente, String nomeMedico, Date data) {
+        Paciente paciente = buscarPacientePorNome(nomePaciente);
+        Medico medico = buscarMedicoPorNome(nomeMedico);
+        if (paciente != null && medico != null) {
+            agendarConsulta(paciente, medico, data);
+        } else {
+            System.out.println("Paciente ou médico não encontrados.");
+        }
+    }
+
     public void buscarConsultas() {
         System.out.println("Relatório de Consultas:");
         for (Consulta consulta : consultas) {
             System.out.println(consulta);
-        }
-    }
-
-    public void exibirInformacoesPessoas() {
-        for (Pessoa pessoa : pessoas) {
-            pessoa.exibirInformacoes();  
         }
     }
 }
