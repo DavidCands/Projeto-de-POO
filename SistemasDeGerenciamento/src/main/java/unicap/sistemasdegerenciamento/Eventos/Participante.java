4befe061df04af7163690a501769ab3bdf6072e3
Danilo Teixeira <104468545+danilot1@users.noreply.github.com>
2024-11-27 21:44:25 -0300

    Update Participante.java

--- SistemasDeGerenciamento/src/main/java/unicap/sistemasdegerenciamento/Eventos/Participante.java
+++ SistemasDeGerenciamento/src/main/java/unicap/sistemasdegerenciamento/Eventos/Participante.java
@@ -1,43 +1,38 @@
 package unicap.sistemasdegerenciamento.Eventos;
 
-import unicap.sistemasdegerenciamento.Pessoa;
-
-class Participante extends Pessoa{
+class Participante {
+    private String nome;
+    private String email;
+    private String telefone;
+    private int idade;
     private boolean isEstudante;
     private boolean isMedico;
     private double precoPago;
     
-    public Participante(String nome, String email, String telefone, int idade, boolean isEstudante, boolean isMedico, double valorEvento) {
-        super(nome, email, telefone, idade);
+    public Participante(String nome, String email, String telefone, int idade, boolean isEstudante, boolean isMedico, double precoPago) {
+        this.nome = nome;
+        this.email = email;
+        this.telefone = telefone;
+        this.idade = idade;
         this.isEstudante = isEstudante;
         this.isMedico = isMedico;
-        if(isEstudante){
-            System.out.println("Desconto para o Estudante aplicado!");
-            this.precoPago = valorEvento * 0.50;
-        }
-        else if(isMedico){
-            System.out.println("Desconto para o Medico aplicado!");
-            this.precoPago = valorEvento * 0.80;
-        }
-        else{
-            this.precoPago = valorEvento;
-        }
-        this.precoPago = valorEvento;
+        this.precoPago = precoPago;
     }
 
-    public void exibirInformacoes(){
-        System.out.println("Informações do Participante:");
-        System.out.println("Nome: "+ getNome());
-        System.out.println("Idade: " + getIdade());
-        System.out.println("E-Mail: "+ getEmail());
-        System.out.print("Profissão: ");
-        if (isEstudante == true){
-            System.out.println("Estudadnte");
-        }
-        else if (isMedico == true){
-            System.out.println("Médico");
-        }
-        System.out.println("Preço a pagar: " +precoPago);
+    public String getNome() {
+        return nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public int getIdade() {
+        return idade;
     }
     
     public double getPrecoPago() {
@@ -49,10 +44,23 @@ class Participante extends Pessoa{
     }
 
     public void exibirDetalhes() {
-        System.out.println("Nome: " + getNome());
-        System.out.println("Email: " + getEmail());
-        System.out.println("Telefone: " + getTelefone());
-        System.out.println("Idade: " + getIdade());
+        System.out.println("Nome: " + nome);
+        System.out.println("Email: " + email);
+        System.out.println("Telefone: " + telefone);
+        System.out.println("Idade: " + idade);
     }
     
+    public void calcularDesconto(double valorEvento) {
+        if(isEstudante){
+            System.out.println("Desconto para o Estudante aplicado!");
+            this.precoPago = valorEvento * 0.50;
+        }
+        else if(isMedico){
+            System.out.println("Desconto para o Medico aplicado!");
+            this.precoPago = valorEvento * 0.80;
+        }
+        else{
+            this.precoPago = valorEvento;
+        }
+    }
 }
